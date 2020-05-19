540c8bdf67a7f7a35bf861fcb01277a25c213a5e
Crow <devcrowmaster@gmail.com>
2020-05-18 20:07:37 -0500

    Updates

---
+++ precache-manifest.72bf0a635fd44bf279c44c484fe60e74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78d274acd03088325a1d754a2610d83",
+    "url": "/breakingbadapp/index.html"
+  },
+  {
+    "revision": "83202e9a760a65898e21",
+    "url": "/breakingbadapp/static/css/main.a745b6e6.chunk.css"
+  },
+  {
+    "revision": "8ed241d35957eb32556c",
+    "url": "/breakingbadapp/static/js/2.5c62a9e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbadapp/static/js/2.5c62a9e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83202e9a760a65898e21",
+    "url": "/breakingbadapp/static/js/main.9fdb7abd.chunk.js"
+  },
+  {
+    "revision": "e0eae92c3a32a7c2f131",
+    "url": "/breakingbadapp/static/js/runtime-main.16eb3b05.js"
+  },
+  {
+    "revision": "6bf365bad9d7f01aa1d384019a0dca92",
+    "url": "/breakingbadapp/static/media/logo.6bf365ba.svg"
+  }
+]);
